da342d990fca9f5fd71d13de706f0f6b74d4019b
da crands <davecrands@gmail.com>
2019-01-19 00:59:17 -0500

    user auth

--- app/__init__.py
+++ app/__init__.py
@@ -3,12 +3,14 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
 from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+bcrypt = Bcrypt(app)
 login = LoginManager(app)
 login.login_view = 'login'
 
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,69 @@
+from flask import render_template, flash, redirect, url_for, request, jsonify
 from flask_login import login_user, logout_user, current_user, login_required
+
 from app import app, db
+from app.models import User
+
 
 @app.route('/')
+@login_required
 def index():
-  return 'Hello World'
+    return 'Hello World'
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        try:
+            username = request.form['username']
+            password = request.form['password']
+        except:
+            return jsonify({
+                'msg': 'Missing form information'
+            })
+            
+        user = User.query.filter_by(username=username).first()                
+        if not user:
+            return jsonify({
+                'msg': 'Invalid uername or password'
+            })
+        
+        if not user.check_password(password):
+            return jsonify({
+                'msg': 'Invalid uername or password'
+            })
+        login_user(user)
+
+    return 'Logged in'
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    '''
+    Expects three form values:
+    - username
+    - password1
+    - password2
+    '''
+    if request.method == 'POST':
+        try:
+            username = request.form['username']
+            password1 = request.form['password1']
+            password2 = request.form['password2']
+        except:
+            return 'Missing part of your form'
+
+        if password1 != password2:
+            return 'Passwords do not match'
+        
+        user = User.query.filter_by(username=username).first()
+        if user:
+            return 'Usernames already exists'
+
+        user = User(username=username)
+        user.set_password(password1)
+        db.session.add(user)
+        db.session.commit()
+        return 'User added'
+
+    return 'test'
