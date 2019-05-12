f9a9ef9b36149e1217a27513e5a772216f1fc7d1
dacrands <davecrands@gmail.com>
2019-05-12 09:23:06 -0400

    Remove preferred-url-scheme from config

--- config.py
+++ config.py
@@ -3,7 +3,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-    PREFERRED_URL_SCHEME = 'https'
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
