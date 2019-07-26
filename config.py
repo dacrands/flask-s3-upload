87662abb9e8bd99b23bd56308b11b9a70816601a
da crands <davecrands@gmail.com>
2019-07-26 19:24:57 -0400

    Add MAX_CONTENT_LENGTH to config

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
+    MAX_CONTENT_LENGTH = 1 * 1024 * 1024
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
