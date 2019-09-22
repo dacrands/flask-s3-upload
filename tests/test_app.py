569b5cec6be6a20e727ae800651c8fa555e6bb51
da crands <davecrands@gmail.com>
2019-09-22 14:18:37 -0400

    Add testing

---
+++ tests/test_app.py
@@ -0,0 +1,47 @@
+import os
+import tempfile
+import boto3
+
+import pytest
+
+from app import app, db
+
+from app.models import User
+from app.email import auth_email, reset_email
+
+@pytest.fixture
+def client():
+  db_fd, app.config['DATABASE'] = tempfile.mkstemp()
+  app.config['TESTING'] = True
+
+  with app.test_client() as client:
+    with app.app_context():      
+      yield client
+  
+  os.close(db_fd)
+  os.unlink(app.config['DATABASE'])
+
+def test_send_email():
+  """Test email"""
+  auth_email_resp = auth_email('welcome@justfiles.com',
+                   'Verify Your Account!',
+                   'test@email.com',
+                   'test')
+  
+  reset_email_resp = reset_email('welcome@justfiles.com',
+                   'Verify Your Account!',
+                   'test@email.com',
+                   'test')
+  
+  assert auth_email_resp == 202
+  assert reset_email_resp == 202
+
+
+def test_user_password():
+  """
+  Test new user
+  """
+  user = User(email="someone@gmail.com", username='This is cool')
+  user.set_password("password")
+  assert user.check_password("password")
+  assert user.password_hash != "password"
