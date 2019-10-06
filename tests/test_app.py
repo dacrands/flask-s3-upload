3682c95181a87503ea4036c6f722c8c2dc367fcf
da crands <davecrands@gmail.com>
2019-10-06 19:54:09 -0400

    Remove services tests from test_app

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,52 +1,28 @@
 import os
 import tempfile
-import boto3
-
 import pytest
 
 from app import app, db
-
 from app.models import User
-from app.email import auth_email, reset_email
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 @pytest.fixture
 def client():
-    db_fd, app.config['DATABASE'] = tempfile.mkstemp()
     app.config['TESTING'] = True
+    app.config['SQLALCHEMY_DATABASE_URI'] =  \
+        'sqlite:///' + os.path.join(basedir, 'test_app.db')
+
+    db.create_all()
 
     with app.test_client() as client:
         with app.app_context():
+            db.create_all()
+
             yield client
 
-    os.close(db_fd)
-    os.unlink(app.config['DATABASE'])
-
-
-def test_send_email():
-    """Test email"""
-
-    auth_email_resp = auth_email('welcome@justfiles.com',
-                                 'Verify Your Account!',
-                                 'test@email.com',
-                                 'test')
-
-    reset_email_resp = reset_email('welcome@justfiles.com',
-                                   'Verify Your Account!',
-                                   'test@email.com',
-                                   'test')
-
-    assert auth_email_resp == 202
-    assert reset_email_resp == 202
-
-
-def test_s3_bucket():
-    """Test configured bucket exists"""
-
-    s3 = boto3.resource('s3')
-    buckets = [bucket.name for bucket in s3.buckets.all()]
-
-    assert app.config['S3_BUCKET'] in buckets
+            db.drop_all()
 
 
 def test_unauthorized_redirect(client):
