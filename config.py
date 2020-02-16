04e35949314e1e7d386ef5e98a11ce6791d1d3a4
da crands <davecrands@gmail.com>
2020-02-16 12:18:41 -0500

    Make max file upload 5GB

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-    MAX_CONTENT_LENGTH = 1 * 1024 * 1024
+    MAX_CONTENT_LENGTH = 5 * 1024 * 1024
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
